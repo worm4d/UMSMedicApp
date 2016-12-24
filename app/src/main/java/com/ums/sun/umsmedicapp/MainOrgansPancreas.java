93e007c60fe95864834639276e6951e097b57f63
Sun <lysun0906@gmail.com>
2016-12-24 22:40:28 +0800

    updated organs part click to details

--- app/src/main/java/com/ums/sun/umsmedicapp/MainOrgansPancreas.java
+++ app/src/main/java/com/ums/sun/umsmedicapp/MainOrgansPancreas.java
@@ -2,12 +2,30 @@ package com.ums.sun.umsmedicapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainOrgansPancreas extends AppCompatActivity {
+
+    private TextView detailsTxt;
+    private ImageView backBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_organs_pancreas);
+
+        detailsTxt = (TextView) findViewById(R.id.details_txt);
+        detailsTxt.setMovementMethod(new ScrollingMovementMethod());
+
+        backBtn = (ImageView) findViewById(R.id.backImage);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
     }
 }
